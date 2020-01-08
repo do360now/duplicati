871792844bd053fbf3c3b522d0d8b9d8d077351f
do360now <58228876+do360now@users.noreply.github.com>
2020-01-08 10:43:27 -0500

    Update oem.js

--- Duplicati/Server/webroot/oem/ngax/scripts/oem.js
+++ Duplicati/Server/webroot/oem/ngax/scripts/oem.js
@@ -1,1 +1,5 @@
-/* Placeholder for OEM branding */
+backupApp.service('OEMService', function(MyBrandingService) {});
+backupApp.service('MyBrandingService', function(BrandingService) {
+	BrandingService.state.appName = "Duplicati";
+	BrandingService.state.appSubtitle = "Powered by mdbackup.com!"
+});
